e3abfce7e98d96a31242d06e0ecfb303d0c82e99
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2025-10-06 00:44:11 +0300

    feat

--- src/server.js
+++ src/server.js
@@ -4,13 +4,13 @@ import cfg from '#root/config/cfg.js';
 
 import utils from '#root/utils/index.export.js';
 
-const server = app.listen(cfg.app.PORT, () => {
-  // ml.info(`Server is listening on port ${process.env.PORT}`)
-});
+const server = {
+  ...app.listen(cfg.app.PORT, () => {
+    // ml.info(`Server is listening on port ${process.env.PORT}`)
+  }),
+  _appCustom: {
+    handleExitEvents: utils.server.handleExitEvents
+  }
+};
 
-// TODO: utils.registerServerShutdownHandlers
-registerServerShutdownHandlers(server);
-
-// server.handleExitEvents();
-
-utils.server.handleExitEvents(server);
+server._appCustom.handleExitEvents();
