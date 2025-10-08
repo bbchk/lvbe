62fcbddb140c5cc16204bc09fede9cc32db029d0
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2025-10-08 14:47:34 +0300

    hello
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be kept; you may remove them yourself if you want to.
    # An empty message aborts the commit.
    #
    # Date:      Wed Oct 8 14:47:34 2025 +0300
    #
    # On branch main
    # Changes to be committed:
    #	modified:   .gitignore
    #	new file:   src/.env.dev.example
    #	modified:   src/db/migrations/index.js
    #

--- src/db/migrations/index.js
+++ src/db/migrations/index.js
@@ -9,3 +9,14 @@
 //     'create_initial_users_and_products_tables',
 //     '4a2b3c1d9e8f70654321abcd'
 // );
+//
+
+
+// IMPORTANT!
+// implement:
+// A migration class contains two methods: up and down. The up method is used to add new tables, columns, or indexes to your database, while the down method should reverse the operations performed by the up method.
+// PROMPT user for prod app_ENV
+//Some migration operations are destructive, which means they may cause you to lose data. In order to protect you from running these commands against your production database, you will be prompted for confirmation before the commands are executed
+
+// Implement step flag as well
+//php artisan migrate:refresh --step=5
